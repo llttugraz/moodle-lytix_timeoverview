499badd57ec44edf4c8bca89954f9da123ef4fe5
Guenther Moser <moser@tugraz.at>
2023-10-10 11:41:45 +0200

    CSS, JS fixes, new version

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023100300; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023101000; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2022112800.00; // Requires this Moodle version 4.1.
 $plugin->component    = 'lytix_timeoverview'; // Full name of the plugin.
 $plugin->dependencies = ['lytix_helper' => ANY_VERSION];
-$plugin->release      = 'v1.1.0';
+$plugin->release      = 'v1.1.1';
 $plugin->supported    = [401, 401];
