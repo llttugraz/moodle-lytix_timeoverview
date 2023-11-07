eab84ba449d3d782787defa71dfb56bfad1707c1
Guenther Moser <moser@tugraz.at>
2023-11-07 12:55:54 +0100

    v1.1.3

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2022112800.00; // Requires this Moodle version 4.1.
 $plugin->component    = 'lytix_timeoverview'; // Full name of the plugin.
 $plugin->dependencies = ['lytix_helper' => ANY_VERSION];
-$plugin->release      = 'v1.1.2';
+$plugin->release      = 'v1.1.3';
 $plugin->supported    = [401, 403];
